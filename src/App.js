5331fffca2879de9153295894b132f4c9e2f811e
RatonRoy <royraton111@gmail.com>
2023-01-22 00:07:58 +0600

    add all the files here

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import './App.css';
-import About from './components/About/About';
-import Home from './components/Home/Home';
-import Icon from './components/Icons/Icon';
-import Navbar from './components/Navbar/Navbar';
+import './App.css'
+import About from './components/About/About'
+import Home from './components/Home/Home'
+import Icon from './components/Icons/Icon'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-     
-=======
-    <div className="">
-      <Navbar/>
-      <Icon/>
-      <Home/> 
-      <About/>
->>>>>>> refs/remotes/origin/main
+    <div className=''>
+      <Navbar />
+      <Icon />
+      <Home />
+      <About />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
